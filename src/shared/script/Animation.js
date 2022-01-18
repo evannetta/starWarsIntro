8d9ef9f465482db38b7fd1a532fcaf731a8a52bc
Anna Yevstihnieieva <anna.yevstihnieieva@mediamonks.com>
2022-01-18 17:56:08 +0200

    small changes

--- src/shared/script/Animation.js
+++ src/shared/script/Animation.js
@@ -21,7 +21,7 @@ export default class Animation extends FrameAnimation {
     .to('.headline', {opacity: 1, duration: 0.2, delay: 1})
     .from('.headline', {scale: 15, duration: 12, ease: "sine.inOut"}, '>')
     .to('.headline', {opacity: 0, duration: 1}, '>-1')
-     // .fromTo('intro-text', .05, {css:{autoAlpha:0}}, {css:{autoAlpha:1}, immediateRender:true})
+    // .fromTo('intro-wrapper', .05, {css:{autoAlpha:0}}, {css:{autoAlpha:1}, immediateRender:true})
     .set('.plane', {css:{rotateX: 45}, ease:Power2.easeOut}, '<-4')
     .to('.intro-text', 50, { top: -550, ease: 'none'}, '<')
     .to('.intro-text', 3, { opacity: 0, ease:Power2.easeOut}, '>-3')
